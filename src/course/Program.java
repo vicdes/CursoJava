54999b1f3663abb2659aa4b862b15d6a7d489bff
vicdes <73723313+vicdes@users.noreply.github.com>
2023-05-12 17:07:53 -0300

    functions

---
+++ src/course/Program.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Formatar: toLowerCase(), toUpperCase(), trim()
+        // Recortar: substring(inicio), substring(inicio, fim)
+        // Substituir: Replace(char, char), Replace(string, string)
+        // Buscar: IndexOf, LastIndexOf
+        // str.Split("")
+    }
+}
